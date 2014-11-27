d417c4fc118fe19230d9c1e66d6a938fa7d5117b
riquelmj <riquelmj@hotmail.com>
2014-11-27 18:15:20 -0300

    primeras tablas en models.py

---
+++ Portaflash/models.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+#from django.db.models.signals import post_save
+#from django.contrib import admin
+#from django.contrib.auth.admin import UserAdmin
+#from django.contrib.auth import authenticate, login
+
+
+class Estado(models.Model):
+	id= models.AutoField('id',primary_key=True)
+	nombreEstado= models.CharField('Nombre Estado', max_length=256, null=False, blank=False)
+
+class OrdenDeCompra(models.Model):
+	numeroOC= models.IntegerField('Numero de Orden de Compra', primary_key=True)
+	nombreEmpresa= models.CharField('Nombre Empresa', max_length=128, null=False, blank=False)
+	rutEmpresa= models.CharField('Rut Empresa', max_length=10, null=False, blank=False)
+	rutVendedor= models.CharField('Rut Vendedor', max_length=10, null=False, blank=False)
+	fechaIngreso= models.DateField('Fecha Ingreso', null=False, blank=False)
+	fechaEntrega= models.DateField('Fecha Entrega', null=False, blank=False)
+
+	#LLaves Foraneas
+	estado = models.ForeignKey(Estado,verbose_name="Estado")
+
+class Despacho(models.Model):
+	id= models.AutoField('id',primary_key=True)
+	fechaDespacho= models.DateTimeField('Fecha Despacho', null=False, blank=False)
+
+class Usuario(models.Model):
+	rutUsuario= models.IntegerField('Rut Usuario', primary_key=True)
+	nombreUsuario= models.CharField('Nombre Usuario', max_length=128, null=False, blank=False)
+	apeliidoUsuario= models.CharField('Apellido Usuario', max_length=128, null=False, blank=False)
+	rol= models.CharField('Rol Usuario', max_length=20, null=False, blank=False)
+
+	#Llaves Foraneas
+	user= models.OneToOneField(User) #Relacion uno a uno con la tabla usuario del sistema
+
--- Portaflash/settings.py
+++ Portaflash/settings.py
@@ -44,10 +44,10 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'Portaflash.urls'
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'Portaflash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PortalflashDB',
+	'USER': 'DB_USER',
+	'PASSWORD': 'DB_PASSWORD',
+	'HOST': 'localhost',
+	'PORT': '3306',
     }
 }
 
